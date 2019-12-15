eee5da5a488e544c3a034636074310ca01c7f07e
Alex Isaienko <thesonerik@gmail.com>
2019-12-15 13:48:02 +0200

    Refactored Rain to use WeatherParticleAnimator

--- digital_clock/lib/layers/weather/rain.dart
+++ digital_clock/lib/layers/weather/rain.dart
@@ -1,82 +1,15 @@
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
+import 'package:digital_clock/layers/weather/weather_particle_animator.dart';
 import 'package:flutter/material.dart';
 
 class Rain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        const raindropStep = 16.0;
-        final raindrops = <Widget>[];
-        for (var x = 0.0; x < constraints.maxWidth; x += raindropStep) {
-          raindrops.add(
-            Padding(
-              padding: EdgeInsets.only(left: x),
-              child: Raindrop(index: x ~/ raindropStep),
-            ),
-          );
-        }
-
-        return Stack(
-          alignment: Alignment.topLeft,
-          children: raindrops,
-        );
-      },
-    );
-  }
-}
-
-class Raindrop extends StatefulWidget {
-  const Raindrop({
-    Key key,
-    @required this.index,
-  }) : super(key: key);
-
-  final int index;
-
-  @override
-  _RaindropState createState() => _RaindropState();
-}
-
-class _RaindropState extends State<Raindrop>
-    with TickerProviderStateMixin<Raindrop> {
-  AnimationController _ctrl;
-  Random _rnd;
-
-  @override
-  void initState() {
-    super.initState();
-    _rnd = Random(widget.index);
-    _ctrl = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 1000 + _rnd.nextInt(1000)),
-    );
-    _ctrl.repeat();
-  }
-
-  @override
-  void dispose() {
-    _ctrl.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final anim = _ctrl.drive(
-      CurveTween(curve: Curves.easeInOut),
-    );
-    return LayoutBuilder(
-      builder: (_, constraints) => AnimatedBuilder(
-        animation: anim,
-        builder: (context, _) => CustomPaint(
-          size: Size(constraints.maxWidth, constraints.maxHeight),
-          painter: RaindropPainter(
-            color: Colors.white,
-            progress: anim.value,
-          ),
-        ),
+    return WeatherParticleAnimator(
+      axis: Axis.vertical,
+      step: 16,
+      particleBuilder: (color, progress) => _RaindropPainter(
+        color: color,
+        progress: progress,
       ),
     );
   }
@@ -85,8 +18,8 @@ class _RaindropState extends State<Raindrop>
 const _raindropWidth = 8.0;
 const _raindropHeight = _raindropWidth * 5;
 
-class RaindropPainter extends CustomPainter {
-  RaindropPainter({
+class _RaindropPainter extends CustomPainter {
+  _RaindropPainter({
     @required this.color,
     @required this.progress,
   });
