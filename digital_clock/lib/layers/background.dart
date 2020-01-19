ee62c8b7f1eb655c94ab7164b49d4273e706bdcd
Alex Isaienko <thesonerik@gmail.com>
2020-01-19 03:59:47 +0200

    Initial background gradient

--- digital_clock/lib/layers/background.dart
+++ digital_clock/lib/layers/background.dart
@@ -1,6 +1,15 @@
+import 'package:digital_clock/clock.dart';
 import 'package:digital_clock/model.dart';
+import 'package:digital_clock/util.dart';
 import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+
+const _dayColors = [
+  Color(0xFF373866), Color(0xFFE0CFD9), Color(0xFF00AFEF), Color(0xFFE0CFD9),
+];
+
+final _topColors = _dayColors;
+final _bottomColors = _dayColors.map((c) => darken(c, 0.25)).toList();
 
 class Background extends StatelessWidget {
   const Background({
@@ -12,11 +21,31 @@ class Background extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ColorTween(begin: Color(0xffD38312), end: Colors.lightBlue.shade900);
-    ColorTween(begin: Color(0xffA83279), end: Colors.blue.shade600);
+    final now = Clock.of(context).now;
+    final timeUpdateRate = Clock.of(context).updateRate;
 
-    return Container(
-      color: Colors.black,
+    final partOfDayLength = 24 / _topColors.length;
+    final partOfDay = now.hour ~/ partOfDayLength;
+    final partOfPartOfDay = (now.hour - partOfDay * partOfDayLength) / partOfDayLength;
+
+    final topColor = _topColors[partOfDay];
+    final nextTopColor = _topColors[(partOfDay + 1) % _topColors.length];
+
+    final bottomColor = _bottomColors[partOfDay];
+    final nextBottomColor = _bottomColors[(partOfDay + 1) % _bottomColors.length];
+
+    final top = Color.lerp(topColor, nextTopColor, partOfPartOfDay);
+    final bottom = Color.lerp(bottomColor, nextBottomColor, partOfPartOfDay);
+
+    return AnimatedContainer(
+      duration: timeUpdateRate,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [top, bottom],
+        ),
+      ),
     );
 
     return FlareActor(
