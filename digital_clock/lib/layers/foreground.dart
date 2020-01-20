9ad02ea04e003571aa90ea3af4ebe7b2d196a0ec
Alex Isaienko <thesonerik@gmail.com>
2020-01-20 03:51:03 +0200

    Foreground mountains

--- digital_clock/lib/layers/foreground.dart
+++ digital_clock/lib/layers/foreground.dart
@@ -1,6 +1,7 @@
+import 'package:digital_clock/clock.dart';
 import 'package:digital_clock/model.dart';
-import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Foreground extends StatelessWidget {
   const Foreground({
@@ -12,23 +13,102 @@ class Foreground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlareActor(
-      'assets/layers.flr',
-      artboard: _artboard(season),
-    );
-  }
-}
+    return LayoutBuilder(builder: (context, constraints) {
+      final w = constraints.maxWidth;
+      final h = constraints.maxHeight * 5 / 7;
 
-String _artboard(Season season) {
-  return 'fg';
-  switch (season) {
-    case Season.spring:
-      return 'fg_spring';
-    case Season.summer:
-      return 'fg_summer';
-    case Season.autumn:
-      return 'fg_autumn';
-    case Season.winter:
-      return 'fg_winter';
+      final c = Clock.of(context);
+
+      final shaderCallback = (Rect bounds) {
+        return LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: c.isDayTime
+              ? [Colors.grey.shade50, Colors.grey.shade400]
+              : [Colors.grey.shade800, Colors.grey.shade900],
+          tileMode: TileMode.mirror,
+        ).createShader(bounds);
+      };
+
+      final blendMode = BlendMode.modulate;
+
+      return Transform.translate(
+        offset: Offset(0, constraints.maxHeight - h),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 0,
+              top: h * 0.1,
+              width: w,
+              height: h,
+              child: ShaderMask(
+                shaderCallback: shaderCallback,
+                blendMode: blendMode,
+                child: SvgPicture.asset(
+                  'assets/mountains/m1.svg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: h * 0.2,
+              width: w,
+              height: h,
+              child: ShaderMask(
+                shaderCallback: shaderCallback,
+                blendMode: blendMode,
+                child: SvgPicture.asset(
+                  'assets/mountains/m2.svg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: h * 0.4,
+              width: w,
+              height: h,
+              child: ShaderMask(
+                shaderCallback: shaderCallback,
+                blendMode: blendMode,
+                child: SvgPicture.asset(
+                  'assets/mountains/m3.svg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: h * 0.5,
+              width: w,
+              height: h,
+              child: ShaderMask(
+                shaderCallback: shaderCallback,
+                blendMode: blendMode,
+                child: SvgPicture.asset(
+                  'assets/mountains/m4.svg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0,
+              top: h * 0.6,
+              width: w,
+              height: h,
+              child: ShaderMask(
+                shaderCallback: shaderCallback,
+                blendMode: blendMode,
+                child: SvgPicture.asset(
+                  'assets/mountains/m5.svg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
 }
