023726a6b25b061cc03c91f0f14c433970395d6c
Alex Isaienko <thesonerik@gmail.com>
2020-01-20 05:14:07 +0200

    Sun gradient

--- digital_clock/lib/elements/sun.dart
+++ digital_clock/lib/elements/sun.dart
@@ -1,3 +1,5 @@
+import 'package:digital_clock/clock.dart';
+import 'package:digital_clock/util/animated_rotation.dart';
 import 'package:flutter/material.dart';
 
 const kSunSize = 120.0;
@@ -5,12 +7,18 @@ const kSunSize = 120.0;
 class Sun extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: kSunSize,
-      height: kSunSize,
-      decoration: ShapeDecoration(
-        color: Colors.yellowAccent,
-        shape: CircleBorder(),
+    return AnimatedRotation(
+      rotation: -Clock.of(context).sunPosition,
+      duration: Clock.of(context).updateRate,
+      child: Container(
+        width: kSunSize,
+        height: kSunSize,
+        decoration: ShapeDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.yellow.shade600, Colors.yellow.shade900],
+          ),
+          shape: CircleBorder(),
+        ),
       ),
     );
   }
