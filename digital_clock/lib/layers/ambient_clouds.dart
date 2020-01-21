5cc97c0d692325cd52cda4ae8d68e5be36de3d57
Alex Isaienko <thesonerik@gmail.com>
2020-01-21 02:29:09 +0200

    Enabled ambient clouds at night

--- digital_clock/lib/layers/ambient_clouds.dart
+++ digital_clock/lib/layers/ambient_clouds.dart
@@ -6,18 +6,12 @@ import 'package:flutter_clock_helper/model.dart';
 class AmbientClouds extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    bool showAmbientClouds;
-    switch (ClockExtra.of(context).weatherCondition) {
-      case WeatherCondition.sunny:
-        showAmbientClouds = true;
-        break;
-      default:
-        showAmbientClouds = false;
-        break;
-    }
-    if (!Clock.of(context).isDayTime) {
-      showAmbientClouds = false;
-    }
+//    final showAmbientClouds = Clock.of(context).isDayTime &&
+//        ClockExtra.of(context).weatherCondition == WeatherCondition.sunny;
+
+    final showAmbientClouds =
+        ClockExtra.of(context).weatherCondition == WeatherCondition.sunny;
+
     return AnimatedSwitcher(
       duration: Clock.of(context).updateRate,
       child: showAmbientClouds ? Clouds(type: CloudType.ambient) : Container(),
